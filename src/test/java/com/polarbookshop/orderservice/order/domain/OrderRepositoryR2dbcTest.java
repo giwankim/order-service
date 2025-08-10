14fdd41a49cc08afb10409a5aba8c4aada9910df
Giwan Kim <giwan.kim@gmail.com>
2025-08-10 12:56:14 +0900

    test(order): add R2dbc tests for OrderRepository with Testcontainers
    - Introduce `OrderRepositoryR2dbcTest` to test repository behavior using R2dbc.
    - Verify functionality for finding orders by ID and saving rejected orders.
    - Update `TestcontainersConfiguration` to be public for visibility in tests.

---
+++ src/test/java/com/polarbookshop/orderservice/order/domain/OrderRepositoryR2dbcTest.java
@@ -0,0 +1,29 @@
+package com.polarbookshop.orderservice.order.domain;
+
+import com.polarbookshop.orderservice.config.DataConfig;
+import com.polarbookshop.orderservice.TestcontainersConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import reactor.test.StepVerifier;
+
+@DataR2dbcTest
+@Import({DataConfig.class, TestcontainersConfiguration.class})
+@Testcontainers
+record OrderRepositoryR2dbcTest(OrderRepository orderRepository) {
+
+  @Test
+  void findOrderByIdWhenNotExisting() {
+    orderRepository.findById(394L).as(StepVerifier::create).expectNextCount(0L).verifyComplete();
+  }
+
+  @Test
+  void createRejectedOrder() {
+    Order rejectedOrder = OrderService.createRejectedOrder("1234567890", 3);
+    orderRepository.save(rejectedOrder)
+        .as(StepVerifier::create)
+        .expectNextMatches(order -> order.status().equals(OrderStatus.REJECTED))
+        .verifyComplete();
+  }
+}
